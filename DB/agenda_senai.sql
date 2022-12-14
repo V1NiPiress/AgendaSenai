eacdd094711c3edcbc8ebf315d1ef76879372724
V1NiPiress <vinipires1411@gmail.com>
2022-12-14 20:10:33 -0300

    DB Att

--- DB/agenda_senai.sql
+++ DB/agenda_senai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Dez-2022 às 03:02
+-- Tempo de geração: 14-Dez-2022 às 23:55
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 7.4.14
 
@@ -42,9 +42,10 @@ CREATE TABLE `contatos` (
 --
 
 INSERT INTO `contatos` (`id`, `nome`, `ddd`, `telefone`, `email`, `cpf`, `endereco`) VALUES
-(1, 'Alessi', '49', '999883982', 'alessi@dmail.com', '87474747441', 'Rua: Chile, 22'),
-(2, 'Tomas', '44', '988836545', 'tomas@fmail.com', '31425671288', 'Rua: Esquina, 24'),
-(4, 'Bolacha da Silva ', '43', '9988732132', 'bola@outlok.com', '87654321982', 'Rua, Fundão 332');
+(1, 'Contato1', '42', '999999999', 'contato1@g.com', '10000000000', 'Rua: Joaquim de Paula Xavier n°1050'),
+(2, 'Contato2', '42', '999999991', 'contato2@g.com', '11111111122', 'Rua: Joaquim de Paula Xavier n°1050'),
+(4, 'Contato3', '42', '929999881', 'contato3@g.com', '99876543213', 'Rua: Joaquim de Paula Xavier n°1050'),
+(7, 'Contato4', '42', '987658493', 'contato4@g.com', '65473876987', 'Rua: Joaquim de Paula Xavier n°1050');
 
 -- --------------------------------------------------------
 
@@ -65,8 +66,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `permissoes`) VALUES
-(1, 'Vini', 'vini@gmail.com', '202cb962ac59075b964b07152d234b70', 'SUPER'),
-(13, 'valmir', 'val@gaamail.com', '7ce3284b743aefde80ffd9aec500e085', 'DELETE');
+(1, 'teste1', 'teste1@g.com', 'c4ca4238a0b923820dcc509a6f75849b', 'SUPER'),
+(13, 'teste2', 'teste2@g.com', 'c81e728d9d4c2f636f067f89cc14862c', 'ADD'),
+(21, 'teste3', 'teste3@g.com', 'eccbc87e4b5ce2fe28308fd9f2a7baf3', 'EDIT'),
+(22, 'teste4', 'teste4@g.com', 'a87ff679a2f3e71d9181a67b7542122c', 'DELETE');
 
 --
 -- Índices para tabelas despejadas
@@ -92,13 +95,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `contatos`
 --
 ALTER TABLE `contatos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
